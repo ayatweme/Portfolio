c6d6b68cb25b68747624588bb825be3257de4c9f
aya_tweme <aya@blueboxjo.com>
2023-11-02 09:35:18 +0200

    update

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Select the preloader element
 var preloader = document.querySelector(".preloader");
 
-// Function to hide the preloader
 function hidePreloader() {
     preloader.style.display = "none";
 }
 
-// Set a timeout to hide the preloader after 5 seconds (5000 milliseconds)
 setTimeout(hidePreloader, 500);
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -26,30 +23,24 @@ document.addEventListener('DOMContentLoaded', function () {
         currentTextIndex = (currentTextIndex + 1) % newTexts.length;
       }
   
-      setTimeout(changeTextContinuously, 100); // Adjust the delay for the letter-by-letter animation
+      setTimeout(changeTextContinuously, 100); 
     }
   
-    // Start the continuous animation
+   
     changeTextContinuously();
   });
-  // Select the navbar element
 var navbar = document.querySelector(".navbar");
 
-// Function to handle the scroll event
 function handleScroll() {
-    // Get the current scroll position
     var scrollPosition = window.scrollY;
 
-    // Define the scroll position where you want the color change to occur (adjust as needed)
-    var colorChangePosition = 100; // Change the value to the desired scroll position
+    var colorChangePosition = 100; 
 
-    // Check if the scroll position is beyond the defined point
     if (scrollPosition > colorChangePosition) {
-        navbar.style.backgroundColor = "#16161e"; // Change the background color to match the body color
+        navbar.style.backgroundColor = "#16161e"; 
     } else {
-        navbar.style.backgroundColor = "transparent"; // Reset to transparent if above the defined point
+        navbar.style.backgroundColor = "transparent"; 
     }
 }
 
-// Attach the scroll event listener
 window.addEventListener("scroll", handleScroll);
